47059f71aff0453149e687bdcbed89b78d49914a
Govind B Chandran <govindchandran235@gmail.com>
2020-08-12 14:00:45 +0530

    Update currencydetect.py

--- currency-detection/currencydetect.py
+++ currency-detection/currencydetect.py
@@ -1,51 +1,68 @@
 import tensorflow.keras
 from PIL import Image, ImageOps
 import numpy as np
+import speech_recognition as sr
+import pyttsx3
 
-# Disabling scientific notation for clarity
+# Disable scientific notation for clarity
 np.set_printoptions(suppress=True)
 
-currency = ['2000', '500','200', '100', '100','50','50']
-
+currency = ["two thousand rupee note", 'five hundred rupee note','two hundred ruppee note', 'one hundred rupee note', 'one hundred rupee note','fifty rupee note','fifty rupee note']
 
 
 # Load the model
 model = tensorflow.keras.models.load_model('keras_model.h5')
 
-# Create the array of the right shape to feed into the keras model
-# The 'length' or number of images you can put into the array is
-# determined by the first position in the shape tuple, in this case 1.
-data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+total = 0
 
-# Replace this with the path to your image
-image = Image.open('test.jpeg')
+while 1:
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    imgfile = input("Enter image name")
+    
+    image = Image.open(imgfile)
 
 #resize the image to a 224x224 with the same strategy as in TM2:
 #resizing the image to be at least 224x224 and then cropping from the center
-size = (224, 224)
-image = ImageOps.fit(image, size, Image.ANTIALIAS)
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
 
 #turn the image into a numpy array
-image_array = np.asarray(image)
+    image_array = np.asarray(image)
 
 # display the resized image
-image.show()
+    image.show()
 
 # Normalize the image
-normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
 
 # Load the image into the array
-data[0] = normalized_image_array
+    data[0] = normalized_image_array
 
 # run the inference
-prediction = model.predict(data)
+    prediction = model.predict(data)
 
-prediction = np.array(prediction)
-index=0
+    prediction = np.array(prediction)
+    index=0
 
-for i in range(0,7):
-  if prediction[0,i]>0.6:
-    index=i
+    for i in range(0,7):
+        if prediction[0,i]>0.6:
+            index=i
 
-print(currency[index])
+    result = (currency[index])
+ 
+    print(result)
+
+
+    def speak(string):
+        eng = pyttsx3.init()
+        eng.say(string)
+        eng.runAndWait()
+    
+    speak(result)
+    
+    speak("if u want to quit press q")
+    inp = input()
+    if inp=='q':
+        break
+
 
